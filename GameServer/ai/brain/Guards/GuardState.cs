b6c43f1937ecb1dbf5eaf3ccbc903116c9127711
Fusseli <121360108+Fusseli@users.noreply.github.com>
2025-08-06 13:50:18 +0200

    New initial upload
    New initial upload due to some compiling errors

--- GameServer/ai/brain/Guards/GuardState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DOL.AI.Brain
-{
-    public class GuardState_RETURN_TO_SPAWN : StandardMobState_RETURN_TO_SPAWN
-    {
-        protected override short Speed => _brain.Body.MaxSpeed;
-
-        public GuardState_RETURN_TO_SPAWN(StandardMobBrain brain) : base(brain) { }
-    }
-}
